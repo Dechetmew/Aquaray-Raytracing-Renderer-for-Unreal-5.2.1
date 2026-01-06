d47973e33b194b37168699b9073893e6bd3babda
ADM8 <63635533+Dechetmew@users.noreply.github.com>
2026-01-06 14:43:00 +0100

    update v0.8.4

--- Engine/Source/Runtime/Renderer/Private/DeferredShadingRenderer.h
+++ Engine/Source/Runtime/Renderer/Private/DeferredShadingRenderer.h
@@ -1,14 +1,5 @@
 //find where RenderRayTracingPrimaryRaysView() is in DeferredShadingRenderer.h, and replace it with this (should be around line 1090) 
-
-	void RenderRayTracingGIgrid(
-		FRDGBuilder& GraphBuilder,
-		const FViewInfo& View,
-		FRDGTextureRef* OutGIProbeDataTexture,
-		int32 SamplePerPixel,
-		int32 HeightFog,
-		float ResolutionFraction,
-		ERayTracingPrimaryRaysFlag Flags);
-
+	
 	void RenderRayTracingPrimaryRaysView(
 		FRDGBuilder& GraphBuilder,
 		const FViewInfo& View,
@@ -17,5 +8,4 @@
 		int32 SamplePerPixel,
 		int32 HeightFog,
 		float ResolutionFraction,
-		ERayTracingPrimaryRaysFlag Flags,
-		FRDGTextureRef GIProbeDataTexture);
+		ERayTracingPrimaryRaysFlag Flags);

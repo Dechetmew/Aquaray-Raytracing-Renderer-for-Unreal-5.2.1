27f5f1767bfe68abdac3b0bc073ab0bd4b91d1f1
ADM8 <63635533+Dechetmew@users.noreply.github.com>
2026-06-07 08:01:15 +0200

    update v13

--- Engine/Source/Runtime/Renderer/Private/DeferredShadingRenderer.h
+++ Engine/Source/Runtime/Renderer/Private/DeferredShadingRenderer.h
@@ -1,11 +1,8 @@
-//find where RenderRayTracingPrimaryRaysView() is in DeferredShadingRenderer.h, and replace it with this (should be around line 1090) 
-	
-	void RenderRayTracingPrimaryRaysView(
-		FRDGBuilder& GraphBuilder,
-		const FViewInfo& View,
-		FRDGTextureRef* OutFinalColorTexture,
-		FRDGTextureRef* OutRayHitDistanceTexture,
-		int32 SamplePerPixel,
-		int32 HeightFog,
-		float ResolutionFraction,
-		ERayTracingPrimaryRaysFlag Flags);
+//Refer to the wiki for Installation
+
+// Insert into Line 1092
+void AquaRayRender(FRDGBuilder& GraphBuilder, const FViewInfo& View, FRDGTextureRef* OutputTexture);
+
+// Insert near line 1154
+static void AquaRayPrepareRayTracing(const FViewInfo& View, TArray<FRHIRayTracingShader*>& OutRayGenShaders);
+static void PrepareRayTracingReflections(...
